de76f9bb45d56ca2d339538a5f34cc08fa555338
abedul59 <70437442+abedul59@users.noreply.github.com>
2026-05-10 19:57:01 +0800

    Create lawMap.js

---
+++ utils/lawMap.js
@@ -0,0 +1,22 @@
+// 檔案路徑： utils/lawMap.js
+
+export const LAW_TABLE_MAP = {
+  '民法': 'civil_law_clauses',
+  '刑法': 'criminal_law_clauses',
+  '中華民國刑法': 'criminal_law_clauses',
+  '民事訴訟法': 'civil_procedure_law_clauses',
+  '刑事訴訟法': 'criminal_procedure_law_clauses',
+  '行政訴訟法': 'administrative_litigation_act_clauses',
+  '行政程序法': 'administrative_procedure_act_clauses',
+  '國家賠償法': 'state_compensation_law_clauses',
+  '公務員服務法': 'public_servants_service_act_clauses',
+  '公務人員保障法': 'public_servants_protection_act_clauses',
+  '憲法': 'constitutional_law_clauses',
+  '中華民國憲法': 'constitutional_law_clauses',
+  '教師法': 'teachers_act_clauses',
+  '教育基本法': 'educational_fundamental_act_clauses',
+  '教師法施行細則': 'teachers_act_enforcement_rules_clauses',
+  '校園霸凌防制準則': 'campus_bullying_prevention_guidelines_clauses',
+  '解聘不續聘停聘或資遣辦法': 'teachers_dismissal_regulations_clauses',
+  '高級中等以下學校教師解聘不續聘停聘或資遣辦法': 'teachers_dismissal_regulations_clauses'
+};
